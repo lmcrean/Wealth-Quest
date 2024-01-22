98903798b81c59f25395b7eb2cbf27405c7f3f52
secarrel <secarrel@outlook.com>
2024-01-22 09:29:55 +0000

    Delay card modal appearing after spin

--- static/js/welcome_modal.js
+++ static/js/welcome_modal.js
@@ -4,21 +4,31 @@ $(document).ready(function () {
 });
 
 // Add an event listener to the "Start Game" button
-document.getElementById("startGameButton").addEventListener("click", function () {
-  // Close the modal when the button is clicked
-  $("#welcomeModal").modal("hide");
-});
+document
+  .getElementById("startGameButton")
+  .addEventListener("click", function () {
+    // Close the modal when the button is clicked
+    $("#welcomeModal").modal("hide");
+  });
 
 // Add event listener to open game card on dice roll.
+
 document.getElementById("spin").addEventListener("click", function () {
-  $("#randomCardModal").modal("show");
+  // Delay the function call by 3 seconds
+  setTimeout(function () {
+    $("#randomCardModal").modal("show");
+  }, 3000); // 3000 milliseconds = 3 seconds
 });
 
 // Add event listener to close game card on accept of decline
-document.getElementById("reject-offer-btn").addEventListener("click", function () {
-  $("#randomCardModal").modal("hide");
-});
+document
+  .getElementById("reject-offer-btn")
+  .addEventListener("click", function () {
+    $("#randomCardModal").modal("hide");
+  });
 
-document.getElementById("accept-offer-btn").addEventListener("click", function () {
-  $("#randomCardModal").modal("hide");
-});
+document
+  .getElementById("accept-offer-btn")
+  .addEventListener("click", function () {
+    $("#randomCardModal").modal("hide");
+  });
